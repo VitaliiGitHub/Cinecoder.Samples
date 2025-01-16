28c9f6db8152377043bf6063dddf6ded41bf172a
Vitaliy Shemet <shemet@cinegy.com>
2025-01-16 19:06:41 +0100

    + update file appveyor.yml

---
+++ common/cinecoder_license_string.cs
@@ -0,0 +1,6 @@
+internal static class License
+{
+    //License valid thru 31/12/2025
+    internal const string Companyname = "Cinegy";
+    internal const string Licensekey = "0123456789";
+}
---
+++ common/cinecoder_license_string.h
@@ -0,0 +1,3 @@
+//License valid thru 31/12/2025
+#define COMPANYNAME "Cinegy"
+#define LICENSEKEY "0123456789"
